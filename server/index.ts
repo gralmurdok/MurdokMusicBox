3cd490f71749bc2c9b89f5a583361734dfe98fef
Luis Tinitana <ljtinitanao@gmail.com>
2022-09-01 02:10:55 -0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,9 @@ app.get("/", (req, res) => {
 });
 
 app.get(Routes.APP_STATUS, (req, res) => {
-  updateAppStatus();
+  if (store.auth.accessToken) {
+    updateAppStatus();
+  }
   res.json(store.status);
 });
 
