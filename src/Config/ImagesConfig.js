ce8a28079e7ed767c03be7d789577e9fa66b4ab3
Luis Tinitana <ljtinitanao@gmail.com>
2022-11-11 14:38:39 -0600

    addresses styles

--- src/Config/ImagesConfig.js
+++ src/Config/ImagesConfig.js
@@ -1,7 +1,7 @@
 const ImagesSection = ({ images, onClickUpdate }) => {
   return (
     <div className="images_section">
-      <button onClick={onClickUpdate}>Actualizar</button>
+      <button onClick={onClickUpdate}>Actualizar Imagenes</button>
       <div className="images_container">
         {images.map((image) => (
           <img className="image_miniature" alt="" src={image.base64Source} />
